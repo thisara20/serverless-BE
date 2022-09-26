dfa06054984a2b26e1624449d534143aeb096b15
Thisara Chandrawansa <75584732+thisara20@users.noreply.github.com>
2022-09-26 15:35:23 +0530

    added login

--- user-service/src/handlers/userHandler.js
+++ user-service/src/handlers/userHandler.js
@@ -1,33 +1,90 @@
-const mysql = require('mysql2');
-const connection = require("./../../db")
-
-
+const mysql = require("mysql2");
+const pool = require("./../../db");
+const bcrypt = require('bcrypt');
+ 
 const DataHandler = {
-  read: async (event, context, callback) => {
+  read: (event, context, callback) => {
+    const query = "SELECT * FROM user";
 
-    var query = "SELECT * FROM user"
-
-    connection.query(query, (error, results) => {
+    pool.execute(query, (error, results) => {
       if (error) {
-        throw new error
+        throw error;
       }
       console.log(results);
+      callback(null, results);
+     // pool.releaseConnection();
+    });
+    
+  },
+};
 
-    })
-    const result = await DataHandler;
+const userHandler = {
+  signup: (req, callback) => {
+    
+    var email= req.body.email; 
+    var name= req.body.name;
+    var password =req.body.password;
+
+    //const query = 'INSERT INTO user (email, name, password) VALUES ('?', ' ?', ' ?')', [email, name,password];
+    
+    pool.query('INSERT INTO user (email, name, password) VALUES ? ? ?', [email, name,password], (error) => {
+      if (error) {
+        throw error;
+      }
+       
+      callback(null, "Inserted successfully");
+      //pool.releaseConnection();
+    });
+  },
+};
+
+const LoginHandler = {
+  login: (req, callback) => {
+    
+    var email= req.body.email;
+    var password =req.body.password;
+    //const query = 'SELECT * FROM user WHERE email=? AND password=? ', [email, password];
+    
+    pool.query('SELECT * FROM user WHERE email=? AND password=? ', [email, password], (error) => {
+      if (error) {
+        throw error;
+      }
+       
+      callback(null, "logged successfully");
+    });
+    /*const result = await DataHandler;
 
     const response = {
 
       statusCode: 200,
       body: JSON.stringify({ results: result }),
+    };
+    callback(null, response);
+     */
+  },
+};
 
+/*
+const DataHandler = {
+  read: async (event, context, callback) => {
 
+    connection.query("SELECT * FROM user" ,function(err,results){
+      if(err) throw err;
+   
+      resolve(results);
+    });
+      
+    
+    const response = {
+      const: result = await DataHandler,
+      statusCode: 200,
+      body: JSON.stringify({results:result}),
     };
     callback(null, response);
   }
 }
-
-
+      
+------signup--
 const userHandler = {
   signup: (event, context, callback) => {
     const response = {
@@ -40,6 +97,5 @@ const userHandler = {
     callback(null, response);
   }
 }
-
-module.exports = { userHandler, DataHandler };
-//export default userHandler;
+*/ 
+module.exports = { userHandler, DataHandler, LoginHandler };
