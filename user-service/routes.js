602a9054c7fde9b057048c5cccf4e052e4fe272d
Thisara Chandrawansa <75584732+thisara20@users.noreply.github.com>
2022-09-26 15:36:11 +0530

    added login

--- user-service/routes.js
+++ user-service/routes.js
@@ -1,8 +1,10 @@
 //import userHandler from "./src/handlers";
 const {userHandler} = require('./src/handlers');
 const { DataHandler } = require('./src/handlers/userHandler');
+const {LoginHandler} =require('./src/handlers/userHandler');
 
  signup = (event,context,callback) => userHandler.signup(event,context,callback );
  read = (event,context,callback) => DataHandler.read(event,context,callback );
+ login = (event,context,callback) => LoginHandler.login(event,context,callback );
 
-module.exports ={signup,read};
+module.exports ={signup,read,login};
