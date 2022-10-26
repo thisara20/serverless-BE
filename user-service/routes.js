2b95c61e1640be4c669e5320ed39fdf800e1b781
thisara20 <chandrawanshathisara@gmail.com>
2022-10-26 10:40:43 +0530

    updated routes and serverless yml

--- user-service/routes.js
+++ user-service/routes.js
@@ -1,27 +1,5 @@
- 
  import {userHandler} from "./src/handlers"; ; 
 
  export const signup = (event,context,callback) => userHandler.signup(event,context,callback );
  export const read = (event,context,callback) => userHandler.read(event,context,callback );
  export const login = (event,context,callback) => userHandler.login(event,context,callback );
- 
-
-//import userHandler from "./src/handlers";
-const {userHandler} = require('./src/handlers');
-const { DataHandler } = require('./src/handlers/userHandler');
-const {LoginHandler} =require('./src/handlers/userHandler');
-
- signup = (event,context,callback) => userHandler.signup(event,context,callback );
- read = (event,context,callback) => DataHandler.read(event,context,callback );
- login = (event,context,callback) => LoginHandler.login(event,context,callback );
-
-module.exports ={signup,read,login};
-
-//import userHandler from "./src/handlers";
-const {userHandler} = require('./src/handlers');
-const { DataHandler } = require('./src/handlers/userHandler');
-
- signup = (event,context,callback) => userHandler.signup(event,context,callback );
- read = (event,context,callback) => DataHandler.read(event,context,callback );
-
-module.exports ={signup,read};
